58bd5bf72522405f46db91cb6933eab9f41a3347
Vinyii <146475782+Vinyii@users.noreply.github.com>
2024-11-22 20:46:28 -0300

    Add files via upload

---
+++ atv02-vinicius-pires-19-11/index.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="cabecario">
+     <div id="d1"></div>
+     <div id="d2">Formulario</div>
+     <div id="d3"></div>
+    </div>
+
+    <nav>
+       <li><a href="https://youtu.be/fJu-G-yMJuw?si=ItmbdvIjldR1Dyq1">neuville</a></li>
+       <li><a href="https://youtu.be/fIkwAkIWPw8?si=PTQISKds6G3d3OKN">arleqina</a></li>
+       <li><a href="https://youtu.be/pOrGdvcxTiM?si=5YQLDSlFpiZfR5n0">fofocas</a></li>
+       <li><a href="https://youtu.be/AyQIAknKNpg?si=zUeqKGKHGyQP_7d1">yellan</a></li>
+    </nav>
+
+
+
+
+    <div class="f">
+    
+    <div id="for">
+      <br>
+    <label for="fname">Nome completo:</label><br>
+    <input type="text" id="oo" name="fname"><br>
+
+    <label for="username">Username:</label><br>
+  <input type="text" id="oo" name="username" required><br>
+
+    <label for="pwd">Senha:</label><br>
+  <input type="password" id="oo" name="pwd"><br><br>
+
+  <label for="email">Coloque o seu email:</label><br>
+  <input type="email" id="oo" name="email"><br>
+
+  <label for="CPF">Digite o seu CPF:</label><br>
+  <input type="tel" id="oo" name="cpf" placeholder="012.345.678-01" pattern="[0-9]{3}.[0-9]{3}.[0-9]{3}.[0-9]{2}" max="11"><br><br>
+
+  
+  <p>Qual o seu genero?</p>
+
+  <input type="radio" id="masculino" name="fav_language" value="Masculino">
+  <label for="masculino">Masculino</label><br>
+
+  <input type="radio" id="feminino" name="fav_language" value="Feminino">
+  <label for="feminino">Feminino</label><br>
+
+  <input type="radio" id="outro genero" name="fav_language" value="Outro genero">
+  <label for="outro genero">Outro genero</label><br><br>
+  
+
+  <label for="date">Informe dua data de nascimento:</label><br><br>
+  <input type="date" id="oo" name="date" ><br><br>
+
+  <label for="quantity">Qual a sua idade:</label><br>
+  <input type="number" id="oo" name="idade" min="0"><br><br>
+
+  <label for="quantity">Digite a rua:</label><br><br>
+  <input type="text" id="oo" name="rua" ><br><br>
+
+  <label for="cel">Numero de telefone:</label><br><br>
+  <input type="nunber" id="ddd" name="ddd" placeholder="11" max="2">
+  <input type="tel" id="oo" name="phone" placeholder="123-45-678" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}" required><br><br>
+
+  <label for="files">Coloque o arquivo pedido:</label><br><br>
+  <input type="file" id="OO" name="files" multiple><br><br>
+
+  <input type="submit" value="Submit" id="e">
+</div>
+
+<div id="for3"></div>
+<div id="for2">
+<p>O que te levou ao nosso site:</p>
+<input type="radio" id="ii" name="fav_language" value="Masculino">
+  <label for="masculino">Por amigos</label><br>
+
+<input type="radio" id="ii" name="fav_language" value="Feminino">
+  <label for="feminino">Ja tinha usado antes</label><br>
+
+  <input type="radio" id="ii" name="fav_language" value="Feminino">
+  <label for="feminino">por curiosidade</label><br>
+
+ <input type="radio" id="ii" name="fav_language" value="Feminino">
+  <label for="feminino">Motivos especiais</label><br>
+
+<input type="radio" id="ii" name="fav_language" value="Outro genero">
+  <label for="outro genero">Ouviu falar por anuncios</label><br><br>
+
+     <p>Qual sua cor favorita:</p>
+
+     <input type="radio" id="ii" name="fav_language" value="Masculino">
+  <label for="masculino">Vermelho</label><br>
+
+<input type="radio" id="ii" name="fav_language" value="Masculino">
+  <label for="masculino">Laranja</label><br>
+
+<input type="radio" id="ii" name="fav_language" value="Masculino">
+  <label for="masculino">Roxo</label><br>
+
+<input type="radio" id="ii" name="fav_language" value="Masculino">
+  <label for="masculino">Verde</label><br>
+
+<input type="radio" id="ii" name="fav_language" value="Masculino">
+  <label for="masculino">Rosa</label><br>
+
+<input type="radio" id="ii" name="fav_language" value="Masculino">
+  <label for="masculino">Azul</label><br>
+
+<input type="radio" id="ii" name="fav_language" value="Masculino">
+  <label for="masculino">Preto</label><br>
+
+<input type="radio" id="ii" name="fav_language" value="Masculino">
+  <label for="masculino">Branco</label><br>
+
+<input type="radio" id="ii" name="fav_language" value="Masculino">
+  <label for="masculino">Violeta</label><br>
+
+<input type="radio" id="ii" name="fav_language" value="Masculino">
+  <label for="masculino">Azul claro</label><br>
+
+<input type="radio" id="ii" name="fav_language" value="Masculino">
+  <label for="masculino">cinza</label><br>
+
+<input type="radio" id="ii" name="fav_language" value="Masculino">
+  <label for="masculino">Cor de bosta</label><br>
+
+<p>Qual o seu passa tempo favorito:</p>
+
+<input type="radio" id="ii" name="fav_language" value="Masculino">
+  <label for="masculino">Lavar louça</label><br>
+
+<input type="radio" id="ii" name="fav_language" value="Masculino">
+  <label for="masculino">Dançar</label><br>
+
+<input type="radio" id="ii" name="fav_language" value="Masculino">
+  <label for="masculino">Cantar</label><br>
+
+<input type="radio" id="ii" name="fav_language" value="Masculino">
+  <label for="masculino">Desenhar</label><br>
+
+<input type="radio" id="ii" name="fav_language" value="Masculino">
+  <label for="masculino">Dormir</label><br>
+
+<input type="radio" id="ii" name="fav_language" value="Masculino">
+  <label for="masculino">Comer</label><br>
+
+<input type="radio" id="ii" name="fav_language" value="Masculino">
+  <label for="masculino">Outro</label><br>
+</div>
+</div>
+
+<div class="rodape">
+  <div id="r1">Copyright &copy 2024 by Vinícius Pires Silveira. Todos os direitos reservados.</div>
+  <div id="r2">
+    <img id="img1" src="https://th.bing.com/th/id/OIP.pVLXabNaXcThC6YYxNWKjQAAAA?w=187&h=333&c=7&r=0&o=5&dpr=1.3&pid=1.7" alt="1">
+  
+  </div>
+</div>
+</body>
+</html>
